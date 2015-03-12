600e028deaba183016d076915619c3dbb2668ec6
jellert <jerzy.ellert@stxnext.pl>
2015-03-12 08:48:02 +0100

    Bug 5705 [Wdrożenie] 5. Szablony (mako); zbudowanie aplikacji presence-analyzer na szablonach Mako; refaktoryzacja kodu

--- src/presence_analyzer/static/js/commons.js
+++ src/presence_analyzer/static/js/commons.js
@@ -3,34 +3,3 @@ function parseInterval(value) {
     result.setMilliseconds(value*1000);
     return result;
 }
-
-function load_users($){
-	var loading = $('#loading');
-	$.getJSON("/api/v1/users", function(result) {
-	    var dropdown = $("#user_id");
-	    $.each(result, function(item) {
-	        dropdown.append($("<option />").val(this.user_id).text(this.name));
-	    });
-	    dropdown.show();
-	    loading.hide();
-	});
-}
-
-function show_user_data($, url, show_user_data_handler){
-	var loading = $('#loading');
-    $('#user_id').change(function(){
-        var selected_user = $("#user_id").val();
-        var chart_div = $('#chart_div');
-        if(selected_user) {
-            loading.show();
-            chart_div.hide();
-           	$.getJSON(url+selected_user, function(result) {
-           		console.log(result);
-           		show_user_data_handler($, result);
-           	});
-
-        }
-    });
-}
-
-
